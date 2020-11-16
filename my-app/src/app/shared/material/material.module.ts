9d2295b03c469d085d0a202f1840a0ec9d9f58b2
Anulfo Ordaz <AROrdaz@ccstenn.com>
2020-11-16 10:45:08 -0600

    Boiler Plate

---
+++ my-app/src/app/shared/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule { }
